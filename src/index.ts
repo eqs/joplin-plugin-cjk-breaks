5c5df6675d9d5aa92b41490f329f6702e1da05e5
eqs <murashige.satoshi.mi1@gmail.com>
2023-09-08 20:24:30 +0900

    Register markdown-it-cjk-breaks plugin

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import joplin from 'api';
+import { ContentScriptType } from 'api/types';
 
 joplin.plugins.register({
-	onStart: async function() {
-		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
-	},
+  onStart: async function() {
+    await joplin.contentScripts.register(
+      ContentScriptType.MarkdownItPlugin,
+      'cjk-breaks',
+      './markdownItCjkBreaks.js'
+    );
+  },
 });
